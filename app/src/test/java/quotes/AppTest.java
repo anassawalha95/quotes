e0ce633935fa510731b82c92dd815903c8b614e5
Anas Sawalha <54035970+anassawalha95@users.noreply.github.com>
2021-04-15 04:54:37 +0300

    Update AppTest.java

--- app/src/test/java/quotes/AppTest.java
+++ app/src/test/java/quotes/AppTest.java
@@ -34,11 +34,36 @@ public class AppTest {
     }
 
     @Test public void testSearchByRandom() throws Exception {
+          String localPath= "src/main/resources/recentquotes.json";
+          String remoteApiPath= "https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en";
         Gson gson =new Gson();
         File file = new File("../app/src/main/resources/recentquotes.json");
         Reader reader=new FileReader(file);
         Quotes[] quotes = gson.fromJson(reader,  Quotes[].class);
         reader.close();
         searchByRandom(quotes);
+
     }
+
+    @Test public void testSearchByRandomRemote() throws Exception {
+
+        String remoteApiPath= "https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en";
+        Gson gson =new Gson();
+        File file = new File("../app/src/main/resources/recentquotes.json");
+        Reader reader=new FileReader(file);
+        Quotes[] quotes = gson.fromJson(reader,  Quotes[].class);
+        reader.close();
+        searchByRandom(quotes);
+
+        int StatusCode =  initializeRemote(remoteApiPath);
+
+        if(StatusCode != 200) {
+
+            System.out.println("Somthing went Wrong From Api A Saved Quote will be generated ");
+            quotes = initialize();
+            System.out.println(searchByRandom(quotes));
+        }
+    }
+
+
 }
